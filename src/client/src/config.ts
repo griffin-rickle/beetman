d1884aed1c36e57fdeff81325b8a0504893c098e
Griff <rickleg.93@gmail.com>
2024-12-20 10:26:59 -0500

    Removed dependency on react-table This introduces kind of a lot of typescript errors but it also removes the dependency on react-table. I'm sure in the future this can be worked around. This commit also fixes whatever issue was happening with the search page, as that works now. It would probably be a good time to look through the codebase and clean up the type BS now.

---
+++ src/client/src/config.ts
@@ -0,0 +1,20 @@
+const CONFIG = {
+    "fields": {
+        "tracks": [
+            "id",
+            "artist",
+            "title",
+            "year",
+            "album",
+            "track"
+        ],
+        "albums": [
+            "id",
+            "artist",
+            "title",
+            "year",
+        ]
+    }
+};
+
+export default CONFIG;
